f2bec1f51dfff27ac1c4fbd3ae5d8927f75b82cb
Akshay Parmar <111117065+AP963R@users.noreply.github.com>
2024-06-23 21:05:14 +0100

    Write custom database

---
+++ index.js
@@ -0,0 +1,12 @@
+const { write } = require("fs");
+const { readDb, writeDb } = require("./db_functions");
+
+const dataObj = {
+    name: 'james',
+    favoriteNumbers: [8, 3]
+}
+
+writeDb(dataObj)
+
+console.log(readDb())
+
